5b7341a8c80b66cc7df8d6306ad67b82533ea62e
saqlainkoser <saqlainkoser@gmail.com>
2024-03-20 18:21:37 +0530

    this is my change

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+
+//functional component
+
+function App() {
+  return (
+    //JSX 
+    <div>
+      <h1>This is my first React app.</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ myapp/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+//functional component
+
+function Navbar() {
+    return (
+      //JSX 
+      <div>
+        <h1>This is my Navbar.</h1>
+      </div>
+    );
+  }
+  
+  export default Navbar;
